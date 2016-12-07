bc4285bfcb36652131744ec5c28c81b3200e1f45
Gregor Brett <gregor.brett@bluefinsolutions.com>
2016-12-07 17:16:23 +0000

    Day 7 part 2

--- lib/Aoc2016/day7.ex
+++ lib/Aoc2016/day7.ex
@@ -17,11 +17,83 @@ defmodule Aoc.Aoc2016.Day7 do
   end
 
   @doc """
+  iex>Aoc.Aoc2016.Day7.part2
+  242
+  """
+  def part2 do
+    {:ok, file} = File.read("./lib/Aoc2016/day7.txt")
+    file
+    |> String.split("\n")
+    |> Enum.reduce(0, fn(line, acc)->
+      case line |> supports_ssl do
+        true -> acc + 1
+        false -> acc
+      end
+    end)
+  end
+
+  @doc """
   iex>Aoc.Aoc2016.Day7.supports_ssl("aba[bab]xyz")
   true
   """
   def supports_ssl(string) do
     [normal_bits, hypernet_seqs] = string |> split_out_hypernet_sequences
+
+    babs = normal_bits
+    |> Enum.reduce([], fn(line, acc)->
+      List.insert_at(acc, 0, line
+      |> get_abas_and_babs
+      |> get_required_babs)
+    end)
+    |> List.flatten
+
+    babs
+    |> Enum.reduce_while( false, fn(bab, acc)->
+      contains = hypernet_seqs |> Enum.reduce_while(false, fn(seq, acc)->
+        if seq |> String.contains?(bab) do
+          acc = true
+          {:halt, true}
+        else
+          acc = false
+          {:cont, false}
+        end
+      end)
+
+      case contains do
+        true -> {:halt, true}
+        false -> {:cont, false}
+      end
+    end)
+  end
+
+  @doc ~S"""
+  iex>Aoc.Aoc2016.Day7.get_required_babs([{"aba", "bab"}])
+  ["bab"]
+  """
+  def get_required_babs(abas_and_babs) do
+    abas_and_babs
+    |> Enum.map(&(elem(&1, 1)))
+  end
+
+  @doc ~S"""
+  Get a list of ABAs and the corresponding BAB values needed
+
+  iex>Aoc.Aoc2016.Day7.get_abas_and_babs("aba")
+  [{"aba", "bab"}]
+  """
+  def get_abas_and_babs(string) do
+    string
+    |> String.codepoints
+    |> Enum.chunk(3,1)
+    |> Enum.map( &(Enum.join(&1)))
+    |> Enum.reduce([], fn(chunk, acc)->
+      [left,middle,right] = chunk |> String.codepoints
+      if left === right and left !== middle do
+        acc |> List.insert_at(0, {chunk, Enum.join([middle, left, middle])})
+      else
+        acc
+      end
+    end)
   end
 
   @doc """
@@ -79,16 +151,4 @@ defmodule Aoc.Aoc2016.Day7 do
       end
     end)
   end
-end
-
-
-ExUnit.start(exclude: [:skip])
-
-defmodule Aoc.Aoc2016.Day7Test do
-  use ExUnit.Case, async: true
-
-  @tag :skip
-  test "test the sample input" do
-  end
-
 end
