d7ed39bef79e9f219161f2b140597dc595ffe0d8
Gregor Brett <gregor.brett@bluefinsolutions.com>
2016-12-08 22:11:37 +0000

    rotate working finally

--- lib/Aoc2016/day8.ex
+++ lib/Aoc2016/day8.ex
@@ -25,20 +25,11 @@ defmodule Aoc.Aoc2016.Day8 do
     build_row(x-1,row |> List.insert_at(0, "🎄"))
   end
 
-  @doc """
-  Display the Tiny Code Displaying Screen
 
-  iex>Aoc.Aoc2016.Day8.display_tcds
-  []
-  """
-  def display_tcds do
-    # build_tcds
-    # |> Enum.each( fn(line)->
-    #   IO.puts line |> List.to_string
-    #   # IO.puts "\n"
-    # end)
-    []
+  def display_tcds(tcds) do
+    tcds |> Matrix.rows
   end
+
   @doc """
   Create a x,y rectangle starting in the top left corner
 
@@ -58,10 +49,94 @@ defmodule Aoc.Aoc2016.Day8 do
   end
 
   def put_into_tcds([], tcds), do: tcds
-  def put_into_tcds(coords, tcds) do
-    [coord | tail] = coords
+  def put_into_tcds([coord | tail], tcds) do
     tcds = put_in tcds[elem(coord, 1)][elem(coord, 0)], "🎅"
     put_into_tcds(tail, tcds)
   end
 
+  def rotate_col(col, rotations, tcds) do
+    rotated_vector = tcds
+    |> Matrix.column(col)
+    |> Vector.to_list
+    |> rotate_a_list(rotations)
+
+    tcds = tcds
+    |> Matrix.rows
+    |> update_rows(col, rotated_vector, length(rotated_vector))
+    |> Matrix.from_rows
+
+    tcds |> Matrix.rows |> IO.inspect
+
+    tcds
+    # IO.inspect tcds
+    # tcds
+    # |> Matrix.rows
+    # |> Enum.map( fn(row)->
+    #   IO.inspect row
+    #   # |> Vector.to_list
+    #   # |> List.replace_at(col, "A")
+    #   # |> Vector.from_list
+    # end)
+    # |> Matrix.from_rows
+    # |> IO.inspect
+    # tcds
+    # |> Matrix.columns
+    #
+    # |> List.replace_at(col, rotated_vector)
+    # |> Matrix.from_rows
+    # |> Matrix.rows
+    # |> Enum.each(fn(row)->
+    #   IO.inspect row
+    # end)
+    # |> Matrix.rotate_clockwise
+    # |> Matrix.rotate_clockwise
+    # |> Matrix.rotate_clockwise
+    # |> IO.inspect
+
+    # tcds
+    # Matrix.from_rows
+
+  end
+  def update_rows(rows, col, new_values, 0), do: rows
+  def update_rows(rows, col, new_values, count) do
+    update_row = length(new_values) - count
+
+    row = rows |> Enum.at(update_row) |> Vector.to_list
+    row = row |> List.replace_at(col, new_values |> Enum.at(update_row))
+    rows = rows |> List.replace_at(update_row, row |> Vector.from_list)
+
+    update_rows(rows, col, new_values, count - 1)
+  end
+
+  @doc """
+  iex>Aoc.Aoc2016.Day8.rotate_a_list(["🎅","🎅","🎄"], 1)
+  ["🎄","🎅","🎅"]
+  iex>Aoc.Aoc2016.Day8.rotate_a_list(["🎄","🎅","🎅"], 1)
+  ["🎅","🎄","🎅"]
+  iex>Aoc.Aoc2016.Day8.rotate_a_list(["🎅","🎅","🎄"], 2)
+  ["🎅","🎄","🎅"]
+  """
+  def rotate_a_list(list,0), do: list
+  def rotate_a_list(list, rotations) do
+    rotate_a_list([(list |> List.last)] ++ (list |> Enum.drop(-1)), rotations - 1)
+  end
+end
+
+ExUnit.start(exclude: [:skip])
+
+defmodule Aoc.Aoc2016.Day8Test do
+  use ExUnit.Case, async: true
+
+  # @tag :skip
+  test "test the col rotation" do
+
+    input = Matrix.new([["🎅","🎅","🎅","🎄","🎄","🎄","🎄"],
+                        ["🎅","🎅","🎅","🎄","🎄","🎄","🎄"],
+                        ["🎄","🎄","🎄","🎄","🎄","🎄","🎄"]],3,7)
+    output = Matrix.new([["🎅","🎄","🎅","🎄","🎄","🎄","🎄"],
+                         ["🎅","🎅","🎅","🎄","🎄","🎄","🎄"],
+                         ["🎄","🎅","🎄","🎄","🎄","🎄","🎄"]],3,7)
+
+    assert Aoc.Aoc2016.Day8.rotate_col(1, 1, input) === output
+  end
 end
